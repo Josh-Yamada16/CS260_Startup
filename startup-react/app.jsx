4598e348c16f51316a1261e5b2abeb773b00462f
Josh Yamada <fourarms216@gmail.com>
2026-02-12 21:30:51 -0700

    finished fleshing out the app routing.

--- startup-react/app.jsx
+++ startup-react/app.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
@@ -7,47 +7,72 @@ import { Index } from './index/index';
 import { Home } from './home/home';
 import { Build } from './build/build';
 import { Other_Builds } from './other_builds/other_builds';
+import { use } from 'react';
 
 export default function App() {
-    return (
-    <BrowserRouter>
-        <div className='body bg-dark text-light'>
-            <header>
-                <h1>DnD Character Builder<sup></sup></h1>
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-                <ul className="nav nav-tabs">
-                    <li className="nav-item">
-                        <NavLink className="nav-link active" aria-current="page" to="/home">Home</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link" to="/build">Build</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link" to="/other_builds">Other Builds</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link" to="/index">Logout</NavLink>
-                    </li>
-                </ul>
-                <hr />
-            </header>
+    function usePageLocation() {
+        const location = useLocation();
+        return location.pathname;
+    }
 
-            <Routes>
-                <Route path='/home' element={<Home />} />
-                <Route path='/build' element={<Build />} />
-                <Route path='/other_builds' element={<Other_Builds />} />
-                <Route path='/index' element={<Index />} />
-            </Routes>
-
-            <footer className="bg-dark text-white-50">
-            <div className="container-fluid">
-                <span className="text-reset">Josh Yamada</span>
-                <a className="text-reset" href="https://github.com/webprogramming260/simon-css">Source</a>
+    function HeaderFooter(){
+        return (
+            <div className='body bg-dark text-light'>
+                <header>
+                    <h1>DnD Character Builder<sup></sup></h1>
+    
+                    <ul className="nav nav-tabs">
+                        <li className="nav-item">
+                            <NavLink className="nav-link active" aria-current="page" to="/home">Home</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/build">Build</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/other_builds">Other Builds</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/index">Logout</NavLink>
+                        </li>
+                    </ul>
+                    <hr />
+                </header>
             </div>
-            </footer>
-        </div>
-    </BrowserRouter>
-    );
+        );
+    }
+
+    function AppContent() {
+        const pathname = usePageLocation();
+        const hideHeader = pathname === '/index' || pathname === '/register';
+        return (
+            <div className={hideHeader ? "d-none" : ""}>
+                {isAuthenticated && !hideHeader && <HeaderFooter />}
+                <Routes>
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/build" element={<Build />} />
+                    <Route path="/other_builds" element={<Other_Builds />} />
+                    <Route path="/index" element={<Index setIsAuthenticated={setIsAuthenticated} />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+
+                <footer className="bg-dark text-white-50">
+                <div className="container-fluid">
+                    <span className="text-reset">Josh Yamada</span>
+                    <a className="text-reset" href="https://github.com/webprogramming260/simon-css">Source</a>
+                </div>
+                </footer>
+            </div>
+        );
+    }
+
+    return (
+        <BrowserRouter>
+            <AppContent />
+        </BrowserRouter>
+    )
+    
 }
 
 function NotFound() {
