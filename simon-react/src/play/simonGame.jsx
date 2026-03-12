476d669dc89fbe6e6f740284d5e33386bbd66fe4
Josh-Yamada16 <fourarms216@gmail.com>
2026-03-12 06:10:29 -0600

    Added the fetch request to the simon game.

--- simon-react/src/play/simonGame.jsx
+++ simon-react/src/play/simonGame.jsx
@@ -36,6 +36,20 @@ export function SimonGame(props) {
     }
   }
 
+  async function saveScore(score) {
+    const date = new Date().toLocaleDateString();
+    const newScore = { name: userName, score: score, date: date };
+    
+    await fetch('/api/score', {
+      method: 'POST', 
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(newScore),
+    });
+
+    // Let other players know the game has concluded
+    GameNotifier.broadcastEvent(userName, GameEvent.End, newScore);
+  }
+
   async function reset() {
     setAllowPlayer(false);
     setPlaybackPos(0);
